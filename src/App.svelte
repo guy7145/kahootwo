7c749155bf928f1dfe3b02c4b934d0d9ba61d86f
Guy <guy7145@gmail.com>
2021-01-02 17:33:50 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import './pages/Home.svelte';
+import Home from './pages/Home.svelte';
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<Home/>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<img src="../public/kahoot-logo.png" alt="logo"/>
